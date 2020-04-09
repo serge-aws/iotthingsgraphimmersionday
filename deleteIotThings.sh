1f3858b89cf09922250d9a897e8ece3fe0ea9796
serge-aws <62933412+serge-aws@users.noreply.github.com>
2020-04-09 11:38:25 +0200

    Update deleteIotThings.sh

--- deleteIotThings.sh
+++ deleteIotThings.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # This script deletes the IoT Core resources that were created during IoT320 
 # workshop session
-cd LAB1
+cd /home/ec2-user/environment/LAB1
+chmod +x *.sh
 ./deleteIotThings.sh
-cd ../LAB2
+cd /home/ec2-user/environment/LAB2
+chmod +x *.sh
 ./deleteIotThings.sh
+cd /home/ec2-user/environment/init_script
